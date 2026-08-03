ae3a4fc2626ff814c8368c00d8e38fab63cf4b3b
Swendon <swendonjepkemoi@gmail.com>
2026-08-03 13:12:16 +0300

    Update landing page branding

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php include 'includes/header.php'; ?>
 
 <section class="landing">
-    <div class="overlay">
-        <h1>Welcome to Equator Royal Tour</h1>
-
-        <p>
-            Sustainable Tourism, Community Development and
-            Environmental Conservation.
-        </p>
-    </div>
 </section>
 
 <?php include 'includes/footer.php'; ?>
